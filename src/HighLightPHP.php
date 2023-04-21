520907a96abbfb3acf4f475b0ce57c9e3086a734
PseudoDevs <johnlester.mychannelnow@gmail.com>
2023-04-21 10:41:24 +0800

    Refactoring the Code

--- src/HighLightPHP.php
+++ src/HighLightPHP.php
@@ -4,30 +4,48 @@ namespace iamjohndev;
 
 class HighLightPHP
 {
+    const HIGHLIGHT_JS_VERSION = '11.7.0';
+    const LANGUAGES_DIR = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/' . self::HIGHLIGHT_JS_VERSION . '/languages';
+    const STYLES_DIR = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/' . self::HIGHLIGHT_JS_VERSION . '/styles';
+
     private $languages_dir;
     private $styles_dir;
     private $highlight_js;
 
     public function __construct()
     {
-        $this->languages_dir = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/languages';
-        $this->styles_dir = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles';
+        $this->languages_dir = self::LANGUAGES_DIR;
+        $this->styles_dir = self::STYLES_DIR;
+        $this->highlight_js = '//cdnjs.cloudflare.com/ajax/libs/highlight.js/' . self::HIGHLIGHT_JS_VERSION . '/highlight.min.js';
     }
 
     public function highlightCode($code, $language, $style)
     {
+        // Validate input
+        if (empty($code) || empty($language) || empty($style)) {
+            throw new InvalidArgumentException('Missing code, language, or style');
+        }
+
         // Load language file
-        $language_file = $this->languages_dir . '/' . $language . '.min.js';
-        $language_content = file_get_contents($language_file);
+        try {
+            $language_file = $this->languages_dir . '/' . $language . '.min.js';
+            $language_content = file_get_contents($language_file);
+        } catch (Exception $e) {
+            throw new Exception('Error loading language file');
+        }
 
         // Load style file
-        $style_file = $this->styles_dir . '/' . $style . '.min.css';
-        $style_content = file_get_contents($style_file);
+        try {
+            $style_file = $this->styles_dir . '/' . $style . '.min.css';
+            $style_content = file_get_contents($style_file);
+        } catch (Exception $e) {
+            throw new Exception('Error loading style file');
+        }
 
         // Execute code highlighting
         $result = '<style>' . $style_content . '</style>';
-        $result .= '<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>';
-        $result .= '<pre><code class="language-' . $language . '">' . htmlentities($code) . '</code></pre>';
+        $result .= '<script src="' . $this->highlight_js . '"></script>';
+        $result .= '<pre><code class="language-' . htmlspecialchars($language) . '">' . htmlspecialchars($code) . '</code></pre>';
         $result .= '<script>document.addEventListener("DOMContentLoaded", (event) => {
             document.querySelectorAll("pre code").forEach((el) => {
               hljs.highlightElement(el);
